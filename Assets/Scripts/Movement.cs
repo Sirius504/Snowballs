3966ce73ec61be27ffd9c0ae657a696449d7da5e
Igorr Polochev <igor@reyomedia.com.cy>
2023-11-27 04:42:25 +0300

    character sprites

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,34 +2,55 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float speed = 1f;
+    enum Direction
+    {
+        Down = 0,
+        Left = 1,
+        Up = 2,
+        Right = 3,
+    }
+
+    private static readonly int DIRECTION_KEY = Animator.StringToHash("direction");
+    private static readonly int SPEED_KEY = Animator.StringToHash("speed");
+
+
+    [SerializeField] private float maxSpeed = 1f;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator _animator;
 
     private Vector2 input;
+    private Direction _direction;
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
             input.y += 1;
+            _direction = Direction.Up;
         }
         if (Input.GetKey(KeyCode.S))
         {
             input.y += -1;
+            _direction = Direction.Down;
         }
         if (Input.GetKey(KeyCode.A))
         {
             input.x += -1;
+            _direction = Direction.Left;
         }
         if (Input.GetKey(KeyCode.D))
         {
             input.x += 1;
+            _direction = Direction.Right;
         }
+        
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = speed * (Vector3)input.normalized;
+        rb.velocity = maxSpeed * (Vector3)input.normalized;
         input = Vector2.zero;
+        _animator.SetInteger(DIRECTION_KEY, (int)_direction);
+        _animator.SetFloat(SPEED_KEY, rb.velocity.magnitude / maxSpeed);
     }
 }
